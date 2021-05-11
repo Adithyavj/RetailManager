aefc160782f6ad4fefc8db9379ab0b25444b6974
Adithya Vijay K <adithyavj@gmail.com>
2021-05-11 18:59:25 +0530

    Add WPF Project with MVVM
    Configures WPF Project with Caliburn Micro for our MVVM Framework. Also Includes Simple Container DI System.

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,51 @@ namespace RMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        //Here we initialize a container
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>(); //Singleton has only one instance for the entire life of the application
+                                                                 //Interface,Implementation
+
+            //Connect ViewModels to Views (we use Reflection to do this GetType()
+            //Get all Types which is a class and endswith ViewModel and register them using container
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
