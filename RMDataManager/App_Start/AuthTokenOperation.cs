2469fc8ca2a4f35652f86958e4f37e6f860f0b5e
Adithya Vijay K <adithyavj@gmail.com>
2021-05-11 12:47:29 +0530

    Add Swagger Documentation to API
    Adds SwashBuckle/Swagger to API
    for documentation and testing.

---
+++ RMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,58 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace RMDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        // New route - /token (AUTH Category)
+        // POST Command which takes in parameters
+        //          * grand_type
+        //          * username
+        //          * password
+        // the output data should come as application/x-www-form-urlencoded
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
