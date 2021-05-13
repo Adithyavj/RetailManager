b26a2e3264534c09395c79d63e5d1808a19aaf63
Adithya Vijay K <adithyavj@gmail.com>
2021-05-13 22:05:20 +0530

    Add LoginView UI
    Designed LoginForm and create properties in
    loginViewModel

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        // property with _ means that it is a backing field meaning
+        // its sole purpose is to supply & hold values for this public property
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+                if (UserName?.Length > 0 && Password?.Length > 0) // ? mark after property checks if it is null
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public void LogIn()
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            // Constructor dependancy injection
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
