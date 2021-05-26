2c968d9e64a360165d23ed08b42fc854c10c08cf
Adithya Vijay K <adithyavj@gmail.com>
2021-05-26 23:32:41 +0530

    Add Foreign Key Relationships
    - Adds Foreign Keys in Tables (Inventory, Sales
    and SaleDetail Tables)

--- RMData/dbo/Tables/SaleDetail.sql
+++ RMData/dbo/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id) 
 )
