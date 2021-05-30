699d31f857a35849f60215514bb0012e2ef8ea8b
Adithya Vijay K <adithyavj@gmail.com>
2021-05-30 19:59:07 +0530

    Upgrading WPF ClassLibrary to .Net Standard 2.0.
    Upgraded the WPF ClassLibrary project to .NET
    Standard 2.0 version.

--- RMDesktopUI.Library/Api/APIHelper.cs
+++ RMDesktopUI.Library/Api/APIHelper.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using RMDesktopUI.Models;
-using RMDesktopUI.Library.Api;
 using RMDesktopUI.Library.Models;
 
 namespace RMDesktopUI.Library.Api
