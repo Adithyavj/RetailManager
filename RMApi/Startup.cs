c568a3aedc38914764b9e5fca1a9f826c9b0a569
Adithya Vijay K <adithyavj@gmail.com>
2021-06-03 15:29:40 +0530

    Moving Security Key to Configuration (appsettings.json)

--- RMApi/Startup.cs
+++ RMApi/Startup.cs
@@ -65,7 +65,7 @@ namespace RMApi
                     jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
